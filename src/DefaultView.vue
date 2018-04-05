ce0e009384fca5aba5cd6ec9e070ce6725278e63
Magnus Jonsson <magnus@hpc2n.umu.se>
2018-04-05 22:34:27 +0200

    cleanup and new stuff

--- src/DefaultView.vue
+++ src/DefaultView.vue
@@ -1,22 +1,22 @@
 
 
 <template>
-  <section class="section">
-    <div class="container">
-      <div class="columns">
-        <div class="column is-9-desktop is-offset-0-desktop">
-          <div class="indrag">
+  <div class="container">
+    <div class="columns">
+      <div class="column is-9-desktop is-offset-0-desktop is-10-touch is-offset-1-touch">
+        <div class="columns">
+          <div class="column is-12 has-text-centered">
             <h1 class="is-size-1">{{page.title}}</h1>
             <h2 class="is-size-4" v-if="page.subtitle">{{page.subtitle}}</h2>
           </div>
+        </div>
           <component v-bind:is="body"></component>
-        </div>
-        <div class="column is-3-desktop is-hidden-touch">
-          <img src="https://www.fot.nu/themes/radio_tower256.png" alt="radio"/>
-        </div>
+      </div>
+      <div class="column is-3-desktop is-hidden-touch has-text-right">
+        <img class="focus-image" src="https://www.fot.nu/themes/radio_tower256.png" alt="radio"/>
       </div>
     </div>
-  </section>
+  </div>
 </template>
 
 <script>
@@ -33,7 +33,7 @@ export default {
   computed: {
     body() {
       return {
-        template: '<div class="content">' + converter.makeHtml(this.page.body) + "</div>"
+        template: '<div class="content has-text-justified-mobile">' + converter.makeHtml(this.page.body) + "</div>"
       }
     }
   },
@@ -42,6 +42,9 @@ export default {
 </script>
 
 <style scoped>
+img.focus-image {
+  padding-top: 0.75rem;
+}
 div.indrag {
   margin-left: 8.33333%;
   padding: 0.75rem;
